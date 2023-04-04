e84d6282f94bb5376e3c03879ffa5bdfcba53876
lilianaplaton <platonliliana1409@gmail.com>
2023-04-05 00:38:37 +0300

    JS lessons

---
+++ JavaScript/script.js
@@ -0,0 +1,47 @@
+/* ALERTS JS
+
+alert("Hello world!") // prefered
+alert(5 + 2);
+window.alert("Hello world!") */
+
+/* JS VARIABLES
+
+var myName = "Eva"
+var yourName = prompt("What is your name?");
+alert("My name is " + myName + ", welcome to my course " + yourName + "!")
+______________________________________________________
+
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your level is currently: " + gameLevel);
+______________________________________________________
+
+function test() {
+    var a = "3";
+    var b = "8";
+
+    var c = a;
+    a = b;
+    b = c;
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+______________________________________________________
+
+//naming allowed
+var myName = "Eva";
+var myvar = "abc";
+var my123 = 123;
+var myname = "Eva"
+var my_name = "Eva"
+var abc123$_ */
+
+/* STRING CONCATENATION 
+var message = "Hello";
+var name = "Eva";
+alert(message + " there, " + name + "!");
+______________________________________________________
+
+*/
